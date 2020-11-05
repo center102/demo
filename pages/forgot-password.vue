ff2650c419d74f5c917639d183b156b606b8756a
tuannt <tuannt.bn@gmail.com>
2020-11-05 10:11:02 +0700

    add auth page

---
+++ pages/forgot-password.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="container">
+    <div class="form-title">
+      Forgot form
+    </div>
+    <el-form ref="form" class="login-form" :model="form" label-width="120px">
+      <el-form-item label="Email">
+        <el-input v-model="form.email" />
+      </el-form-item>
+
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">
+          Submit
+        </el-button>
+        <el-button @click="back">
+          back
+        </el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  layout: 'auth',
+  data () {
+    return {
+      form: {
+        email: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit () {
+      console.log('submit!')
+    },
+    back () {
+      this.$router.back()
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ pages/login.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="container">
+    <div class="form-title">
+      Login form
+    </div>
+    <el-form ref="form" class="login-form" :model="form" label-width="120px">
+      <el-form-item label="Email">
+        <el-input v-model="form.email" />
+      </el-form-item>
+      <el-form-item label="Password">
+        <el-input v-model="form.password" />
+      </el-form-item>
+
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">
+          Login
+        </el-button>
+        <el-button @click="gotoForgot">
+          Forgot pass
+        </el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  layout: 'auth',
+  data () {
+    return {
+      form: {
+        email: '',
+        pass: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit () {
+      this.$router.push('/')
+    },
+    gotoForgot () {
+      this.$router.push('forgot-password')
+    }
+  }
+})
+</script>
